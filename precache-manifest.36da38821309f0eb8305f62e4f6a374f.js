b4e334b015ea397d1645d71427f618ff13e9372b
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-11-22 20:59:09 -0600

    Updates

---
+++ precache-manifest.36da38821309f0eb8305f62e4f6a374f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a768f7b9a5bf1c58bb3f169eb3f6e0",
+    "url": "/ESThing64/my-portfolio/index.html"
+  },
+  {
+    "revision": "eb76129eef93b80bff6f",
+    "url": "/ESThing64/my-portfolio/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "7b4de096065d67136aa9",
+    "url": "/ESThing64/my-portfolio/static/js/2.796ec0c6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/ESThing64/my-portfolio/static/js/2.796ec0c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20dbf8761f4103cd3f07",
+    "url": "/ESThing64/my-portfolio/static/js/3.b0f512d9.chunk.js"
+  },
+  {
+    "revision": "eb76129eef93b80bff6f",
+    "url": "/ESThing64/my-portfolio/static/js/main.c113c55c.chunk.js"
+  },
+  {
+    "revision": "bf6b17e02db55e2eddda",
+    "url": "/ESThing64/my-portfolio/static/js/runtime-main.89475045.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/ESThing64/my-portfolio/static/media/logo.06e73328.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ESThing64/my-portfolio/precache-manifest.36da38821309f0eb8305f62e4f6a374f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ESThing64/my-portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
