8efa66d85598627b28226740bd88ec144d11a79e
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-11-23 20:19:51 -0600

    added code from pervious portfolio

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default function NavBar(){
+    return(
+
+<nav>
+<ul className="nav-bar">
+  <li className="nav-item"> <a href="#top">me. </a> </li>
+  <li className="nav-item"> <a href="#project-link">projects.</a> </li>
+  <li className="nav-item"> <a href="#contact">contact.</a> </li>
+  <li className="nav-item"> <a href="https://drive.google.com/file/d/1c7XqwMbNXsNmM4cM9u2Jmq-DUB2k5df1/view?usp=sharing">resume.</a> </li>
+
+
+</ul>
+</nav>
+
+    )
+}
